7d1a165b59d08a1a29f8eb2972db806db30ab481
Viktoriia <zalevskaya3103@mail.ru>
2023-01-05 11:07:24 +0100

    Убирает лишний nav, делает открытое меню по умолчанию без js, добавляет состояния в variables

--- source/js/index.js
+++ source/js/index.js
@@ -1,16 +1,13 @@
-const menuButtonOpen = document.querySelector('#menu-button-open')
-const menuButtonClose = document.querySelector('#menu-button-close')
-
-const mobileMenu = document.querySelector('#mobile-menu');
+const menuButtonOpen = document.querySelector('#header-menu-button');
+const mobileMenu = document.querySelector('#header-menu');
 
 console.log(`menuButtonOpen: `, menuButtonOpen);
-console.log(`menuButtonClose: `, menuButtonClose);
 console.log(`mobileMenu: `, mobileMenu);
+
+menuButtonOpen.classList.remove('main-button--nojs');
+mobileMenu.classList.remove('main-nav--nojs');
 
 menuButtonOpen.addEventListener('click', function () {
   mobileMenu.classList.toggle('hidden');
-});
-
-menuButtonClose.addEventListener('click', function () {
-  mobileMenu.classList.toggle('hidden');
+  menuButtonOpen.classList.toggle('main-header__button-active');
 });
